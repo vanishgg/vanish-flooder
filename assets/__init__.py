1266c2cde27380cad1555d39812b27c300069d0e
Virtual <aydurunsu@gmail.com>
2024-09-01 10:11:22 +0100

    Create __init__.py

---
+++ assets/__init__.py
@@ -0,0 +1,14 @@
+import os 
+from colorama import Style
+import discord
+import asyncio
+from datetime import datetime
+import random
+from gradientify import Colors
+
+c = "\033[38;2;194;255;182m"
+d = "\033[1;90m"
+red = "\033[1;31m"
+blue = "\033[1;34m"
+w = "\033[1;37m"
+r = Style.RESET_ALL
