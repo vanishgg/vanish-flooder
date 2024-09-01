17a7baec11f8d2cc985feae8b9dfa03e0968c8c8
Virtual <aydurunsu@gmail.com>
2024-09-01 10:15:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,16 @@
-# ~~ Imports ~~ #   
+from assets import *
+from assets.utility.util import *
 
-import discord
-import asyncio
-from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
-from colorama import Fore, Style
-import os
-import random
-from datetime import datetime
 
-# ~~ Colors ~~ #
-
-c = "\033[38;2;25;25;112m"
-d = "\033[1;90m"
-red = "\033[1;31m"
-blue = "\033[1;34m"
-w = "\033[1;37m"
-r = Style.RESET_ALL
-
-# ~~ Source (do not mess with unless you know what you are doing) ~~ #
-
-emj = "stuff/emojis.txt"
-
-def tkns():
-    if os.path.exists('input/tokens.txt'):
-        with open('input/tokens.txt', 'r') as file:
-            tokens = [line.strip() for line in file.readlines()]
-            return tokens, len(tokens)
-    return [], 0
-
-tokens, tok = tkns()
-
-def cc():
-        os.system('cls')
-
-def emo():
-    if os.path.exists(emj):
-        with open(emj, 'r', encoding='utf-8') as file:
-            emojis = [line.strip() for line in file.readlines()]
-            return emojis
-    return []
-
-emojis = emo()
 
 async def spam(token, uid, message, count, rs, rm, em):
-    intents = discord.Intents.all()
-    client = discord.Client(intents=intents)
+    inte = discord.Intents.all()
+    client = discord.Client(intents=inte)
 
     @client.event
     async def on_ready():
-        await asyncio.sleep(1)
-        print(f"{datetime.now().strftime(f'{c}[{r}{d}%H{r}{w}:{r}{d}%M{r}{w}:{r}{d}%S{r}{c}]{r}')}    {blue}[+]{Style.RESET_ALL}      {d}->{r}    {c}Logged in as{r} {d}{client.user.name}{r}")
+        await asyncio.sleep(0.5)
+        print(f"{datetime.now().strftime(f'{blue}[{r}{d}%H{r}{w}:{r}{d}%M{r}{w}:{r}{d}%S{r}{blue}]{r}')}    {w}[{r}{blue}/{r}{w}]{r}      {d}->{r}    {Colors.blue_to_white(f'Logged in as')} ({d}{client.user.name}{r})")
         await asyncio.sleep(1)
         tar = await client.fetch_user(int(uid))
 
@@ -64,10 +25,10 @@ async def spam(token, uid, message, count, rs, rm, em):
 
                 await tar.send(msg)
                 await asyncio.sleep(0.5)
-                print(f"{datetime.now().strftime(f'{c}[{r}{d}%H{r}{w}:{r}{d}%M{r}{w}:{r}{d}%S{r}{c}]{r}')}    {blue}[+]{Style.RESET_ALL}      {d}->{r}    {c}Sent DM{r} {red}~{r} {message} ({d}{client.user.name}{r})")
+                print(f"{datetime.now().strftime(f'{c}[{r}{d}%H{r}{w}:{r}{d}%M{r}{w}:{r}{d}%S{r}{c}]{r}')}    {w}[{c}+{r}{w}]{r}      {d}->{r}    {Colors.mint(f'Sent DM ~ {message}')} ({d}{client.user.name}{r})")
 
             except discord.Forbidden:
-                print(f"{datetime.now().strftime(f'{c}[{r}{d}%H{r}{w}:{r}{d}%M{r}{w}:{r}{d}%S{r}{c}]{r}')}  {red}[-]{Style.RESET_ALL}      {d}->{r}    {red}{tar.name} DMs are closed{r} ({d}{client.user.name}{r})")
+                print(f"{datetime.now().strftime(f'{red}[{r}{d}%H{r}{w}:{r}{d}%M{r}{w}:{r}{d}%S{r}{red}]{r}')}  {w}[{red}-{r}{w}]{r}      {d}->{r}    {Colors.red_to_white(f'{tar.name} DMs are closed')} ({d}{client.user.name}{r})")
                 continue
 
     await client.start(token)
@@ -75,50 +36,38 @@ async def spam(token, uid, message, count, rs, rm, em):
 
 
 
-
-def logo():
-    nn = Center.XCenter(f"""
-{c}
-                                                        
- _____         _     _      _____ _           _         
-|  |  |___ ___|_|___| |_   |   __| |___ ___ _| |___ ___ 
-|  |  | .'|   | |_ -|   |  |   __| | . | . | . | -_|  _|
- \___/|__,|_|_|_|___|_|_|  |__|  |_|___|___|___|___|_|   
- {r}
-    """)
-    print(nn)
+    
 
 async def main():
-    os.system('title Vanish Flooder')
-    cc()
+    tokens = load_tokens()
+    set_t(f'Vanish Flooder ~ Loaded {len(tokens)} tokens')
+    clr()
     logo()
-    print(f'                                               {w}Loaded:{r} {c}‹{r}{w}{tok}{r}{c}›{r} {w}tokens') 
-    print(f"""
-                                            {c}[{r}1{c}]{r} {w} Dm Spam     {c}[{r}2{c}]{r} {w} Exit
-    """) 
     option = input(f'            {c}[~] $Choice ~{r} ')
 
     if option == "1":
         print()
         tokens = open('input/tokens.txt', 'r').read().splitlines()
-        uid = input(Colorate.Horizontal(Colors.blue_to_white, "Vanish@cmd$~ → User Id ~ "))
+
+
+        uid = input(Colors.mint("Vanish@cmd$~ → User Id ~ "))
         if uid == '':
             await main()
-        message = input(Colorate.Horizontal(Colors.blue_to_white, "Vanish@cmd~  → Message ~ "))
+        message = input(Colors.mint("Vanish@cmd~  → Message ~ "))
         if message == '':
             await main()
-        count = int(input(Colorate.Horizontal(Colors.blue_to_white, "Vanish@cmd$~ → Amount ~ ")))
+        count = int(input(Colors.mint("Vanish@cmd$~ → Amount ~ ")))
         if count == '':
             await main()        
-        rs = input(Colorate.Horizontal(Colors.blue_to_white, "Vanish@cmd$~ → Random String (y/n) ~ ")).strip().lower() == 'y'
+        rs = input(Colors.mint("Vanish@cmd$~ → Random String (y/n) ~ ")).strip().lower() == 'y'
         if rs == '':
             await main()        
-        rm = input(Colorate.Horizontal(Colors.blue_to_white, "Vanish@cmd$~ → Random Emojis (y/n) ~ ")).strip().lower() == 'y'
+        rm = input(Colors.mint("Vanish@cmd$~ → Random Emojis (y/n) ~ ")).strip().lower() == 'y'
         if rm == '':
             await main()
         em = 5
         if rm:
-            em = int(input(Colorate.Horizontal(Colors.blue_to_white, "Vanish@cmd$~ → Emoji Amount ~ ")))
+            em = int(input(Colors.mint("Vanish@cmd$~ → Emoji Amount ~ ")))
         if em == '':
             await main()
 
